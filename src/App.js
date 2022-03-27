ade3d20102e995884a24becc39ec4000dcdc8f2d
James Michael <jomichael1994@hotmail.com>
2022-03-27 23:19:34 +0100

    state management

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await axios.get('https://api.github.com/users');
+    this.setState({
+      users: response.data,
+      loading: false
+    });
+  }
+
   render() {
     return (
       <div>
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
